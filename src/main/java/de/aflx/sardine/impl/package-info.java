4f5ad085aeb0deb40a24fcd99cf3a83f5a4533f6
Artem Kubiria <akubirya@gmail.com>
2018-07-11 09:27:25 +0200

    added sardine-module with okhttp dependency

--- src/main/java/de/aflx/sardine/impl/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Assembles classes only related to the {@link ch.boye.httpclientandroidlib.HttpClient} implementation.
- */
-package de.aflx.sardine.impl;
--- src/main/java/de/aflx/sardine/util/Logger.java
+++
@@ -1,7 +0,0 @@
-package de.aflx.sardine.util;
-
-public class Logger {
-	public void warn(String log) {
-//		Log.i("Sardine", log);
-	}
-}
